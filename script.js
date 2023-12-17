940ec70b83aa3fbc82a6c6505a1195a5f8665b9b
Alex Evdokimov <a.evdokimov@tg.dunice.net>
2023-12-17 16:05:38 +0300

    start

---
+++ script.js
@@ -0,0 +1,16 @@
+const dataForRender = {
+  main: {
+    title: [
+      { id: 1, subtilte: "Я Ашот Кузнецов", mainTitle: true },
+      { id: 2, subtilte: "Мои фото", mainTitle: false },
+      { id: 3, subtilte: "Интересные для меня видео", mainTitle: false },
+    ],
+    img: [],
+    content: 
+    `<div class="contentContainer">
+
+    </div>`
+  },
+  work: { title: "", img: [], content: "" },
+  qual: { title: "", img: [], content: "" },
+};
